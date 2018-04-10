d4c1265a6571ddc3dc73fb5e53564d17d90ea941
A. Scott Tribble <a.scott.tribble@gmail.com>
2018-04-09 21:17:12 -0700

    adjusted closeModal for real

--- index.js
+++ index.js
@@ -4,22 +4,39 @@
 
 
 
-function cubbieListener(){
-  $('.cubbie').click(function () {
-    if($('.found')){
+function cubbieListener() {
+  $('.cubbie').click(function (event) {
+    event.stopPropagation();
+    if ($('.found')) {
       $('.found').removeClass('found');
-    }  
+    }
     $(this).next('div').toggleClass('found');
   });
 }
 
 
 
-function closeModal(){
+function closeModal() {
+
+
+  $('html').click(function () {
+    if (!$(this).closest('.found').length) {
+      $('.found').removeClass('found');
+    }
+  });
+
+
+  $('.content').click(function (e) {
+    e.stopPropagation();
+  });
+
+
   $('.close').click(function () {
     $(this).closest('.found').removeClass('found');
-  })
+  });
+
 }
+
 
 
 $(() => {
